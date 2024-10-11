7c7cc4b16e89d754080f4d23e33594c0cf65d0cd
eltaeif-houssem <houssem.developer@gmail.com>
2024-10-11 20:21:31 +0100

    add base server connection

---
+++ server/src/server.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+import connectDB from "@configs/db.config";
+
+// define vars
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+const PORT = process.env.PORT || 8080;
+
+// init middlewares
+app.use(express.json({ limit: "3mb" }));
+app.use(express.urlencoded({ extended: true, limit: "3mb" }));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+// add health check for the api
+app.get("/api/v1/health-check", (req, res) => {
+  res.status(200).send({ message: "it work's" });
+});
+
+// handle database and server app running
+connectDB()
+  .then(() => {
+    server.listen(PORT);
+  })
+  .catch(() => {});
