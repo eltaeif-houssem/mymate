fb0d5287cb1ed8277e185cf1c90338a22ed6ce27
eltaeif-houssem <houssem.developer@gmail.com>
2024-10-12 23:27:37 +0100

    adjust user service

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -1,47 +1,54 @@
 import User from "@models/user.model";
 import { CustomError } from "@utils/errors.util";
 
-// create user
-export const createUser = async (payload: any) => {
-  const userExist = await User.findOne({ email: payload.email });
+class UserService {
+  // create user
+  async createUser(payload: any) {
+    const userExist = await User.findOne({ email: payload.email });
 
-  if (userExist) throw new CustomError("User already exists", 400);
+    if (userExist) throw new CustomError("User already exists", 400);
 
-  const newUser = await User.create(payload);
-  const { password, ...data } = newUser.toObject();
+    const newUser = await User.create(payload);
+    const { password, ...data } = newUser.toObject();
 
-  return data;
-};
+    return data;
+  }
 
-// find user
-export const findUser = async (payload: any) => {
-  const userExist = await User.findOne({ email: payload.email });
+  // find user
+  async findUser(payload: any) {
+    const userExist = await User.findOne({ email: payload.email });
 
-  if (!userExist) throw new CustomError("User does not exist", 404);
+    if (!userExist) throw new CustomError("User does not exist", 404);
 
-  const { password, ...data } = userExist.toObject();
+    const { password, ...data } = userExist.toObject();
 
-  return data;
-};
+    return data;
+  }
 
-// update user
-export const updateUser = async (id: string, payload: any) => {
-  const updatedUser = await User.findByIdAndUpdate(id, payload, { new: true });
+  // update user
+  async updateUser(id: string, payload: any) {
+    const updatedUser = await User.findByIdAndUpdate(id, payload, {
+      new: true,
+    });
 
-  if (!updatedUser) throw new CustomError("User was not found", 400);
+    if (!updatedUser) throw new CustomError("User was not found", 400);
 
-  const { password, ...data } = updatedUser.toObject();
+    const { password, ...data } = updatedUser.toObject();
 
-  return data;
-};
+    return data;
+  }
 
-// delete user
-export const deleteUser = async (id: string) => {
-  const deletedUser = await User.findByIdAndDelete(id, { new: true });
+  // delete user
+  async deleteUser(id: string) {
+    const deletedUser = await User.findByIdAndDelete(id, { new: true });
 
-  if (!deletedUser) throw new CustomError("User was not found", 400);
+    if (!deletedUser) throw new CustomError("User was not found", 400);
 
-  const { password, ...data } = deletedUser.toObject();
+    const { password, ...data } = deletedUser.toObject();
 
-  return data;
-};
+    return data;
+  }
+}
+
+export const userService = new UserService();
+export default userService;
