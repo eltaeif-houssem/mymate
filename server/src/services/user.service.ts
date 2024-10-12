4bf21cee5a6befc2e568f4d91c6fce1490e02af1
eltaeif-houssem <houssem.developer@gmail.com>
2024-10-12 23:11:42 +0100

    add find user service

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -12,3 +12,14 @@ export const createUser = async (payload: any) => {
 
   return data;
 };
+
+// find user
+export const findUser = async (payload: any) => {
+  const userExist = await User.findOne({ email: payload.email });
+
+  if (!userExist) throw new CustomError("User does not exist", 404);
+
+  const { password, ...data } = userExist.toObject();
+
+  return data;
+};
