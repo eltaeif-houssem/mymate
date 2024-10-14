14f9e74c1aa3cfa127ac3f7be89b1183094a9184
eltaeif-houssem <houssem.developer@gmail.com>
2024-10-14 18:00:07 +0100

    add reset password controller

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -4,6 +4,10 @@ import * as jwtUtil from "@utils/jwt.util";
 import * as bcryptUtil from "@utils/bcrypt.util";
 import { CustomError } from "@utils/errors.util";
 import { IUserReq } from "@interfaces/request.interface";
+import { readTemplate } from "@utils/template.util";
+import { generateOTP } from "@utils/email.util";
+import emailService from "@services/email.service";
+import Otp from "@models/otp.model";
 
 // signup user
 export const signup = async (
@@ -123,6 +127,36 @@ export const resetPasswordEmail = async (
 
     const user = await userService.findUser(body);
     if (!user) throw new CustomError("User does not exist", 404);
+
+    const otp = generateOTP();
+    await Otp.create({
+      otp,
+      userId: user._id,
+      verified: false,
+      expiresAt: new Date(Date.now() + 86400000),
+    });
+
+    const htmlTemplate = readTemplate("otp.template.html");
+    htmlTemplate.replace("{{OTP_CODE}}", otp);
+    htmlTemplate.replace(
+      "{{RESET_LINK}}",
+      "http://localhost:3000/auth/reset-password"
+    );
+    htmlTemplate.replace(
+      "{{CURRENT_YEAR}}",
+      new Date().getFullYear().toString()
+    );
+
+    await emailService.send(
+      `${body.email}`,
+      "password reset",
+      "",
+      htmlTemplate
+    );
+
+    response
+      .status(201)
+      .send({ message: "an reset password code was submitted to your email" });
   } catch (error) {
     next(error);
   }
