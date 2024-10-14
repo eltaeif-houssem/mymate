0c47218ee8dc74bc625d5dff367ce9752e4a7204
eltaeif-houssem <houssem.developer@gmail.com>
2024-10-14 16:28:57 +0100

    add add send email service

--- server/src/services/email.service.ts
+++ server/src/services/email.service.ts
@@ -1,6 +1,23 @@
 import transporter from "@configs/email.config";
+import { getEmailBody } from "@utils/email.util";
+import { CustomError } from "@utils/errors.util";
 
-class EmailService {}
+class EmailService {
+  async send(
+    to: string | string[],
+    subject: string,
+    text: string = "",
+    html: string = ""
+  ) {
+    const body = getEmailBody(to, subject, text, html);
+    try {
+      const response = await transporter.sendMail(body);
+      return response.response;
+    } catch (error) {
+      throw new CustomError("Orror occurred while sending the email");
+    }
+  }
+}
 
 export const emailService = new EmailService();
 export default emailService;
