b469ec50968f5632e8f536f25922d4093ecf10b6
eltaeif-houssem <houssem.developer@gmail.com>
2024-10-14 17:20:15 +0100

    add otp model

---
+++ server/src/models/otp.model.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const otpSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  otp: { type: Number, required: true },
+  expiresAt: { type: Date, required: true },
+  verified: { type: Boolean, default: false },
+});
+
+export const Otp = mongoose.model("Otp", otpSchema);
+export default Otp;
