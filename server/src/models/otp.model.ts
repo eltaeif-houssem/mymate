8f5dae30a0c72427591b09edac2774fd018b5ed9
eltaeif-houssem <houssem.developer@gmail.com>
2024-10-15 12:23:42 +0100

    add verify otp service

--- server/src/models/otp.model.ts
+++ server/src/models/otp.model.ts
@@ -1,15 +1,20 @@
 import mongoose from "mongoose";
 
-const otpSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
+const otpSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    otp: { type: Number, required: true },
+    expiresAt: { type: Date, required: true },
+    verified: { type: Boolean, default: false },
   },
-  otp: { type: Number, required: true },
-  expiresAt: { type: Date, required: true },
-  verified: { type: Boolean, default: false },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export const Otp = mongoose.model("Otp", otpSchema);
 export default Otp;
--- server/src/services/otp.service.ts
+++ server/src/services/otp.service.ts
@@ -1,5 +1,6 @@
 import Otp from "@models/otp.model";
 import { generateOTP } from "@utils/email.util";
+import { CustomError } from "@utils/errors.util";
 
 class OtpService {
   async createOtp(uid: string) {
@@ -11,6 +12,21 @@ class OtpService {
       expiresAt: new Date(Date.now() + 86400000),
     });
     return otp;
+  }
+
+  async verifyOtp(payload: any) {
+    const otp = await Otp.findOne(payload);
+    const today = new Date();
+
+    if (!otp) {
+      throw new CustomError("there's no otp");
+    }
+
+    if (today > otp.expiresAt) {
+      throw new CustomError("otp code is expired");
+    }
+
+    await Otp.findOneAndUpdate(payload, { verified: true });
   }
 }
 
