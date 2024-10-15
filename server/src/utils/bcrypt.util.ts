25dd1e0a4ad358ea28b5c57ecc808ce2340e222f
eltaeif-houssem <houssem.developer@gmail.com>
2024-10-15 13:13:46 +0100

    add hash password util function

--- server/src/utils/bcrypt.util.ts
+++ server/src/utils/bcrypt.util.ts
@@ -1,5 +1,12 @@
 import bcrypt from "bcrypt";
 
+// hash password
+export const hashPassword = async (password: string) => {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = bcrypt.hash(password, salt);
+  return hashedPassword;
+};
+
 // compare password
 export const comparePassword = async function (
   password: string,
