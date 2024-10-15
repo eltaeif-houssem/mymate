0704fb7e881c23c5784e6a520c48d421281c6197
eltaeif-houssem <houssem.developer@gmail.com>
2024-10-15 12:42:54 +0100

    add check otp service

--- server/src/services/otp.service.ts
+++ server/src/services/otp.service.ts
@@ -32,6 +32,21 @@ class OtpService {
 
     await Otp.findOneAndUpdate(payload, { verified: true });
   }
+
+  async checkOtp(payload: any) {
+    const otp = await Otp.findOne(payload);
+    const today = new Date();
+
+    if (!otp) {
+      throw new CustomError("there's no otp");
+    }
+
+    if (today > otp.expiresAt) {
+      throw new CustomError("otp code is expired");
+    }
+
+    return otp;
+  }
 }
 
 export const otpService = new OtpService();
