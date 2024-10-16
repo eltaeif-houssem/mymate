f6afd3d2170a63c2bd6e6dfd75d3bee247340559
eltaeif-houssem <houssem.developer@gmail.com>
2024-10-16 11:39:27 +0100

    add auth store

--- client/src/store/auth.store.ts
+++ client/src/store/auth.store.ts
@@ -1,26 +1,8 @@
 import { makeAutoObservable } from "mobx";
-import { IAuth } from "@interfaces/authStore.interface";
 
 class AuthStore {
-  auth: IAuth = {
-    isLoggedIn: false,
-    user: null,
-  };
-
   constructor() {
     makeAutoObservable(this);
-  }
-
-  async signup() {}
-
-  async signin() {}
-
-  async signout() {
-    localStorage.removeItem("access_token");
-    this.auth = {
-      isLoggedIn: false,
-      user: null,
-    };
   }
 }
 
