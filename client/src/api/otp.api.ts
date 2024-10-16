92a29d60b76861ad1ffbb3721303fa7ce1ea18e7
eltaeif-houssem <houssem.developer@gmail.com>
2024-10-16 11:21:26 +0100

    adjust otp api

--- client/src/api/otp.api.ts
+++ client/src/api/otp.api.ts
@@ -1,7 +1,8 @@
 import axios from "./axios.api";
+import { IVerifyOtp } from "@interfaces/otp.interface";
 
 export const sendOtp = async (email: string) =>
   axios.post("/otp/send-otp", { email });
 
-export const verifyOtp = async (body: any) =>
+export const verifyOtp = async (body: IVerifyOtp) =>
   axios.post("/otp/verify-otp", body);
