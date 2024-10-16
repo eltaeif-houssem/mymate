02eb3fd40b7cc27dcbf4ed237687660f5c4a612a
eltaeif-houssem <houssem.developer@gmail.com>
2024-10-16 12:35:06 +0100

    add route filtering based on auth status'

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,29 @@
 import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import routes from "@routes/index.route";
+import { useAppContext } from "./context/context";
 
 const App: React.FC = () => {
+  const ctx = useAppContext();
   return (
     <div className="w-full min-h-full">
       <Suspense fallback={<p>Loading...</p>}>
         <Routes>
-          {routes.map((route) => (
-            <Route path={route.path} element={route.element} />
-          ))}
+          {routes
+            .filter((route) => {
+              if (route.path === "*") {
+                return true;
+              }
+
+              if (route.auth === ctx.authStore.auth.isLoggedIn) {
+                return true;
+              }
+
+              return false;
+            })
+            .map((route) => (
+              <Route path={route.path} element={route.element} />
+            ))}
         </Routes>
       </Suspense>
     </div>
