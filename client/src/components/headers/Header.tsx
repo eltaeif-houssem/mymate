3e8ac934a27f7dd7cecd1630579fbdd1657e0fb8
eltaeif-houssem <houssem.developer@gmail.com>
2024-10-20 20:19:27 +0100

    add header

--- client/src/components/headers/Header.tsx
+++ client/src/components/headers/Header.tsx
@@ -5,6 +5,8 @@ import { Link } from "react-router-dom";
 import SearchTextField from "../textfields/SearchTextField";
 import * as routePaths from "@constants/route-urls.constant";
 
+import profilePic from "@assets/profile-1.png";
+
 interface Props {
   value: string;
   onClick: () => void;
@@ -26,6 +28,26 @@ const Header: React.FC<Props> = (props) => {
           onClick={onClick}
           onChange={onChange}
         />
+      </div>
+
+      <div className="flex items-center">
+        <div className="flex mr-10 cursor-pointer hover:opacity-80 duration-200">
+          <img src={profilePic} alt="profile-logo" className="w-12" />
+          <div className="ml-3">
+            <p className="text-md font-semibold">Steve Rogers</p>
+            <p className="text-xs">@steve_rogers</p>
+          </div>
+        </div>
+
+        <div className="flex items-center">
+          <div className="bg-white py-2 px-3 rounded-md mr-4 cursor-pointer hover:opacity-80 duration-200">
+            <i className="fa-solid fa-gear"></i>
+          </div>
+
+          <div className="bg-white py-2 px-3 rounded-md cursor-pointer hover:opacity-80 duration-200">
+            <i className="fa-solid fa-bell"></i>
+          </div>
+        </div>
       </div>
     </div>
   );
