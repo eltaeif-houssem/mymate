62216b97ac966c9197a80884ec16d193f034ac6f
eltaeif-houssem <houssem.developer@gmail.com>
2024-10-21 21:01:12 +0100

    add user routes

--- client/src/constants/route-urls.constant.ts
+++ client/src/constants/route-urls.constant.ts
@@ -3,5 +3,11 @@ export const AUTH_SIGNUP = "/auth/signup";
 export const AUTH_SIGNIN = "/auth/signin";
 export const AUTH_RESET_PASSWORD = "/auth/reset-password";
 
+/*==== Defina app routes ====*/
+export const USER_PROFILE = "/user/profile";
+export const USER_PROFILE_DETAILS = "/user/profile/:id";
+export const USER_FRIENDS = "/user/friends";
+export const USER_SETTINGS = "/user/settings";
+
 /*==== Define other routes ====*/
 export const HOME = "/";
