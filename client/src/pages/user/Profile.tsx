c8f61eb707df549c357ef24b76b44dedc7de2ea6
eltaeif-houssem <houssem.developer@gmail.com>
2024-10-24 23:55:03 +0100

    add header to profile page

--- client/src/pages/user/Profile.tsx
+++ client/src/pages/user/Profile.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "@components/headers/Header";
 
 const Profile: React.FC = () => {
-  return <div className="w-full h-screen bg-blue-50">Profile</div>;
+  return (
+    <div className="w-full h-screen bg-blue-50">
+      <Header value="" onChange={() => {}} onClick={() => {}} />
+    </div>
+  );
 };
 
 export default Profile;
