4cf8abdfe4c366ff0992d419c16576887fe70a82
eltaeif-houssem <houssem.developer@gmail.com>
2024-10-25 22:24:19 +0100

    add avatar and cover fetching controllers

--- server/src/controllers/profile.controller.ts
+++ server/src/controllers/profile.controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Response } from "express";
 import { CustomError } from "@utils/errors.util";
 import { IUserReq } from "@interfaces/request.interface";
 import profileService from "@services/profile.service";
+import path from "path";
 
 export const updateAvatar = async (
   request: IUserReq,
@@ -42,6 +43,38 @@ export const updateCover = async (
     );
 
     response.status(201).send({ message: "cover picture was updated" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const fetchAvatar = async (
+  request: IUserReq,
+  response: Response,
+  next: NextFunction
+) => {
+  const { id } = request.params;
+  const imagePath = path.join(__dirname, "../uploads/avatars", `${id}`);
+  try {
+    response.sendFile(imagePath, (err) => {
+      if (err) throw new CustomError("avatar does not exist", 400);
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const fetchCovers = async (
+  request: IUserReq,
+  response: Response,
+  next: NextFunction
+) => {
+  const { id } = request.params;
+  const imagePath = path.join(__dirname, "../uploads/covers", `${id}`);
+  try {
+    response.sendFile(imagePath, (err) => {
+      if (err) throw new CustomError("cover does not exist", 400);
+    });
   } catch (error) {
     next(error);
   }
