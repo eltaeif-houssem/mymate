f9ea3c62b2892485777642a19815dcacc2d14b8d
eltaeif-houssem <houssem.developer@gmail.com>
2024-10-30 13:22:20 +0100

    add find user profile service

--- server/src/services/profile.service.ts
+++ server/src/services/profile.service.ts
@@ -1,6 +1,16 @@
 import Profile from "@models/profile.model";
 
 class ProfileService {
+  async findProfile(filter: any) {
+    let profileExists = await Profile.findOne(filter);
+
+    if (!profileExists) {
+      profileExists = await Profile.create(filter);
+    }
+
+    return profileExists;
+  }
+
   async updateUserAvatar(filter: any, filename: string) {
     let profileExists = await Profile.findOne(filter);
 
