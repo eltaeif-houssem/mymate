91bc9a3d0bff0289edcc44cb8502324558447ed5
eltaeif-houssem <houssem.developer@gmail.com>
2024-10-30 13:30:58 +0100

    add fetch profile service

--- client/src/services/profile.service.ts
+++ client/src/services/profile.service.ts
@@ -1,6 +1,15 @@
 import * as profileApi from "@api/profile.api";
 
 class ProfileService {
+  async fetchProfile(id: string, access_token: string) {
+    try {
+      const { data } = await profileApi.getProfile(id, access_token);
+      return data;
+    } catch (error: any) {
+      return { error: error.response.data.message };
+    }
+  }
+
   async updateAvatar(body: any, id: string) {
     try {
       const { data } = await profileApi.updateAvatar(body, id);
